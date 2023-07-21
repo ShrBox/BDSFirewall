8718b0b74c7fa0b9ae3f436828e9dc1a2357e130
ShrBox <shrbox@outlook.com>
2023-07-22 01:07:41 +0800

    refactor: finish LoginFlood detection

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -17,18 +17,17 @@ extern Logger logger;
  */
 std::unordered_map<std::string, unsigned short> LoginPacketTries;
 
-void PluginInit()
-{
-    std::thread([]{
+void PluginInit() {
+    std::thread([] {
         while (!ll::isServerStopping()) {
             LoginPacketTries.clear();
-            std::this_thread::sleep_for(std::chrono::seconds(20));
+            std::this_thread::sleep_for(std::chrono::seconds(1));
         }
     }).detach();
     logger.info("Firewall enabled!");
 }
 
-std::string splitAddress(std::string& address) {
+std::string splitAddress(std::string &address) {
     auto pos = address.find(':');
     if (pos != std::string::npos) {
         return address.substr(0, pos);
@@ -36,38 +35,42 @@ std::string splitAddress(std::string& address) {
     return {};
 }
 
-struct RakPacket
-{
-    RakNet::SystemAddress systemAddress;
-    RakNet::RakNetGUID guid;
-    unsigned int length;
-    uint32_t bitSize;
-    unsigned char* data;
-    bool deleteData;
-    bool wasGeneratedLocally;
-};
+//struct RakPacket
+//{
+//    RakNet::SystemAddress systemAddress;
+//    RakNet::RakNetGUID guid;
+//    unsigned int length;
+//    uint32_t bitSize;
+//    unsigned char* data;
+//    bool deleteData;
+//    bool wasGeneratedLocally;
+//};
 
-void RakAddToBanList(RakNet::RakPeer* _this, std::string& address, unsigned int time) {
-    SymCall("?AddToBanList@RakPeer@RakNet@@UEAAXPEBDI@Z", void, RakNet::RakPeer*, const char*, unsigned int)(_this, address.c_str(), time);
+void RakAddToBanList(RakNet::RakPeer *_this, std::string &address, unsigned int time) {
+    SymCall("?AddToBanList@RakPeer@RakNet@@UEAAXPEBDI@Z", void, RakNet::RakPeer*, const char*, unsigned int)(_this,
+                                                                                                             address.c_str(),
+                                                                                                             time);
 }
-// MotdFlood protection
-TInstanceHook(RakPacket*, "?Receive@RakPeer@RakNet@@UEAAPEAUPacket@2@XZ", RakNet::RakPeer) {
-    RakPacket* pkt = original(this);
-    if (pkt && (pkt->data[0] == 0x01 || pkt->data[0] == 0x07 || pkt->data[0] == 0x09)) {
-        std::string address = pkt->systemAddress.ToString(false, 124);
+// LoginFlood protection
+THook(bool,
+      "?ProcessOfflineNetworkPacket@RakNet@@YA_NUSystemAddress@1@PEBDHPEAVRakPeer@1@PEAVRakNetSocket2@1@PEA_N_K@Z",
+      RakNet::SystemAddress systemAddress, const char *data, const int length, RakNet::RakPeer *rakPeer,
+      void *rakNetSocket, bool *isOfflineMessage, void *timeRead) {
+    bool rtn = original(systemAddress, data, length, rakPeer, rakNetSocket, isOfflineMessage, timeRead);
+    if ((unsigned char)data[0] == 0x07) {
+        std::string address = systemAddress.ToString(false, 124);
         if (!address.empty()) {
 //            if (BlackList::query(address)) {
 //                return pkt;
 //            }
             LoginPacketTries[address] = ++LoginPacketTries[address];
             logger.debug("IP: {} Tries: {}", address, LoginPacketTries[address]);
-            if (LoginPacketTries[address] > 25) {
-                logger.warn("MotdFlood detected! IP: {}", address);
-                RakAddToBanList(this, address, 0);
+            if (LoginPacketTries[address] >= 5) {
+                logger.warn("LoginFlood detected! IP: {}", address);
+                RakAddToBanList(rakPeer, address, 0);
                 logger.warn("IP: {} has been added into BlackList", address);
-                return pkt;
             }
         }
     }
-    return pkt;
+    return rtn;
 }
